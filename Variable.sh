cb6d110fd50f81927735d528cd16ae5e3c8a7ab0
Eryngiv <a.klozevits@gmail.com>
2025-12-30 23:04:46 -0600

    Add Variable.sh script

---
+++ Variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+gastos=150
+echo "El nombre de este script es $0"
+echo "El número de identificación del proceso es $$"
+echo "Estamos trabajando con el shell $SHELL"
+echo "La variable declarada al inicio tiene como valor $gastos"
+echo "Tiempo total de ejecución de este script $SECONDS"
+
+
