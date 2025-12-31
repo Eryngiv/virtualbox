bf7ae71522b6f4c005507f8ce844cc95f1e5371f
Eryngiv <a.klozevits@gmail.com>
2025-12-31 14:42:14 -0600

    Add Llamar_parámetros.sh script

---
+++ Llamar_parámetros.sh
@@ -0,0 +1,16 @@
+
+#!/bin/bash
+
+nombre=$1
+cultivo=$2
+superficie=$3
+zona=$4
+
+echo "Productora: $nombre"
+echo "Cultivo: $cultivo"
+echo "Superficie: $superficie"
+echo "Zona: $zona"
+
+echo "La productora $1 es candidata para créditos en la zona de $4"
+echo ".....(2025)......"
+
