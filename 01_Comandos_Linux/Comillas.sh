d7b1a7fb24c6f5162ce6d980ad6acc2d898440d7
Eryngiv <a.klozevits@gmail.com>
2025-12-31 18:28:03 -0600

    Organize scripts into 01_Comandos_Linux folder

---
+++ 01_Comandos_Linux/Comillas.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+nombre="Juan Pérez"
+
+echo "Hola $nombre, ten buen día"
+echo 'Hola $nombre, ten un buen día'
+
---
+++ 01_Comandos_Linux/Detalles.sh
@@ -0,0 +1,7 @@
+
+#!/bin/bash
+
+echo "Hola hoy es $(date)"
+echo "El usuario se llama $USER"
+echo "La ruta al directorio principal es $HOME"
+echo "Hasta luego, vuelve pronto"
